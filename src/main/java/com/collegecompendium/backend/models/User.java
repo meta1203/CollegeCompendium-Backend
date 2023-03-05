3af64ceee8393168a1b6a9c3d12521fcd4e9d424
Erik N. <Erik.Noll@student.nmt.edu>
2023-03-04 18:00:58 -0700

    Chngd to abstract/moved unique fields to student or college

--- src/main/java/com/collegecompendium/backend/models/User.java
+++ src/main/java/com/collegecompendium/backend/models/User.java
@@ -35,33 +35,33 @@ import lombok.NoArgsConstructor;
 // `user` is an H2 reserved word, so force this
 // table to be called `users` instead
 @Table(name = "users")
-public class User {
+public abstract class User {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	@Default
     private final String id = UUID.randomUUID().toString();
+	
 	@NotNull
 	@Column(length = 256, unique = true)
 	private String auth0Id;
+	
 	@NotNull
 	@Email
 	@Column(length = 1024)
     private String email;
+	
 	@NotNull
 	@NotEmpty
 	@Column(length = 128, unique = true)
 	private String username;
+	
+	@NotEmpty
 	@NotNull
-	@Column(length = 128)
-    private String firstName;
+	@Size(min = 10, max = 120)
 	@NotNull
-	@Column(length = 128)
-    private String lastName;
-	@Column(length = 16)
-	@Size(max = 16)
-	private String middleInitial;
-	@NotNull
-    private String location;
+	@Column(length = 120)
+	private String location;
 	
 	// gets everything auth0 knows about the given JWT token
 	// returns a HashMap because there's no documentation I can find
@@ -86,18 +86,6 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 	public String getLocation() {
 		return location;
 	}
@@ -114,13 +102,5 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public String getMiddleInitial() {
-		return middleInitial;
-	}
-
-	public void setMiddleInitial(String middleInitial) {
-		this.middleInitial = middleInitial;
 	}
 }
