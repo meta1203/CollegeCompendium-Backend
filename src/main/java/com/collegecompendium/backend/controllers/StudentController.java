2d642928100546d0c42569d16e6bfd4a52ae7a74
Hunter Hancock <hancock.hunter@gmail.com>
2023-03-28 13:40:46 -0600

    add permissions when creating new objects

--- src/main/java/com/collegecompendium/backend/controllers/StudentController.java
+++ src/main/java/com/collegecompendium/backend/controllers/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.collegecompendium.backend.configurations.Auth0Provider;
 import com.collegecompendium.backend.models.Student;
 import com.collegecompendium.backend.repositories.StudentRepository;
 
@@ -25,6 +26,9 @@ public class StudentController {
 	
 	@Autowired
 	private StudentRepository studentRepository;
+	
+	@Autowired
+	private Auth0Provider auth0Provider;
 	
 	@PostMapping("/student")
 	public Student createNewStudent(@RequestBody Student input, @AuthenticationPrincipal Jwt token, HttpServletResponse response) {
@@ -44,6 +48,9 @@ public class StudentController {
 
 		// Saving the student to the db
 		Student output = studentRepository.save(input);
+		
+		// add student permission to auth0
+		auth0Provider.addPermissionToUser(output, "student");
 		return output;
 	}
 
