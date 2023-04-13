e6f244f7b15a13147e0d1bc3073bd73ac45add4e
Hunter Hancock <dr.miles.manners.ss@gmail.com>
2023-04-13 12:55:51 -0600

    Major type needed @Data and @Entity, enums need @Enumerated annotation

--- src/main/java/com/collegecompendium/backend/controllers/DegreeController.java
+++ src/main/java/com/collegecompendium/backend/controllers/DegreeController.java
@@ -1,17 +1,9 @@
 package com.collegecompendium.backend.controllers;
 
-import com.collegecompendium.backend.models.Major;
-import com.collegecompendium.backend.repositories.DegreeRepository;
-import com.collegecompendium.backend.repositories.MajorRepository;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.collegecompendium.backend.repositories.DegreeRepository;
 
 @RestController
 public class DegreeController {
--- src/main/java/com/collegecompendium/backend/models/Degree.java
+++ src/main/java/com/collegecompendium/backend/models/Degree.java
@@ -1,15 +1,12 @@
 package com.collegecompendium.backend.models;
 
-import com.collegecompendium.backend.repositories.DegreeRepository;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import jakarta.persistence.*;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * This class represents a college degree. It contains the name of the degree and the number of credits required to earn it.
@@ -20,27 +17,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Data
 @Entity
 public class Degree {
-    private enum DegreeType {
-        ASSOCIATE,
-        BACHELOR,
-        MASTER,
-        DOCTORATE
-    }
+	private enum DegreeType {
+		ASSOCIATE,
+		BACHELOR,
+		MASTER,
+		DOCTORATE
+	}
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
 
-    @NotEmpty
-    @NotNull
-    @ManyToOne
-    private Major major;
+	@NotNull
+	@ManyToOne
+	private Major major;
 
-    // These actually can be empty/null, if the school doesn't provide enough data
-    private DegreeType degreeType;
-    private int creditsRequired;
+	// These actually can be empty/null, if the school doesn't provide enough data
+	private DegreeType degreeType;
+	private int creditsRequired;
 
-
-
-    }
+}
 
